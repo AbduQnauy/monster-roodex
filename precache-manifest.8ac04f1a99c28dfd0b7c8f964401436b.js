a599111ca14ca8873005bf9f7953781bda276c80
Abdu Qnauy <abduqnauy@gmail.com>
2019-10-31 04:19:12 +0200

    Updates

---
+++ precache-manifest.8ac04f1a99c28dfd0b7c8f964401436b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733e337532635dc355be4f61cef87147",
+    "url": "/monster-roodex/index.html"
+  },
+  {
+    "revision": "2a50acea5bac801bb569",
+    "url": "/monster-roodex/static/css/main.a86a3aab.chunk.css"
+  },
+  {
+    "revision": "fb79491648a461a135b1",
+    "url": "/monster-roodex/static/js/2.6e0c7d1d.chunk.js"
+  },
+  {
+    "revision": "2a50acea5bac801bb569",
+    "url": "/monster-roodex/static/js/main.86ea6154.chunk.js"
+  },
+  {
+    "revision": "50ec80ac85859b3b80ab",
+    "url": "/monster-roodex/static/js/runtime-main.4a453848.js"
+  }
+]);
